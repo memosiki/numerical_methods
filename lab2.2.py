bbf0deb1db96a89f326404ed54e181dab981f82a
memosiki <russiankoteyka@gmail.com>
2019-05-13 06:14:40 +0300

    Добавить обоснование невозможности работы метода простой итерации

--- lab2.2.py
+++ lab2.2.py
@@ -30,12 +30,29 @@ def f2_diff_x2(x1, x2):
 
 # чтобы было удобнее выражать x1 и x2
 # поменяет равенства местами в исходной системе
-def phi2(x1, x2):
-    return (4 - x1 ** 2)**0.5
-
 
 def phi1(x1, x2):
     return (math.e ** x2) - 2
+
+
+def phi2(x1, x2):
+    return (4 - x1 ** 2) ** 0.5
+
+
+def phi1_diff_x1(x1, x2):
+    return 0.
+
+
+def phi1_diff_x2(x1, x2):
+    return math.e ** x2
+
+
+def phi2_diff_x1(x1, x2):
+    return - x1 / (4 - x1 ** 2) ** 0.5
+
+
+def phi2_diff_x2(x1, x2):
+    return 0.
 
 
 def newton_system(x0):
@@ -75,7 +92,21 @@ def newton_system(x0):
     return x, iter_count
 
 
+def norm(x, x_prev):
+    return max(x[0] - x_prev[0], x[1] - x_prev[1])
+
+
 def simple_iter_system(x0):
+    x1, x2 = x0[0][0], x0[1][0]
+    q = max(
+        abs(phi1_diff_x1(x1, x2)) + abs(phi1_diff_x2(x1, x2)),
+        abs(phi2_diff_x1(x1, x2)) + abs(phi2_diff_x2(x1, x2)))
+
+    if q >= 1:
+        print("В заданной области не выполняется условие сходимости итерационного процесса")
+        print('q = ', q)
+        return mc.zeroes(2, 1), 0
+
     # Матрица функций
     phi = Matrix([
         [phi1],
@@ -97,12 +128,12 @@ def simple_iter_system(x0):
 
 
 eps = float(input())
-bound_a, bound_b = 1, 1  # начальные значения
+bound_a, bound_b = 1.54, 1.26  # начальные значения
 x0 = Matrix([[bound_a], [bound_b]])
 x, iter_count = newton_system(x0)
 print("Найденное значение Методом Ньютона: \n", x)
 print("Количество итераций: ", iter_count)
 print()
 x, iter_count = simple_iter_system(x0)
-print("Метод простой итераций", x)
+print("Метод простой итераций \n", x)
 print("Количество итераций: ", iter_count)
