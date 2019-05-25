a437323cbec086c8ff6f0204c03c437f0d640d6a
memosiki <russiankoteyka@gmail.com>
2019-05-25 20:31:48 +0300

    Переделать 3.4 с помощью многочлена Ньютона

--- lab3_4.py
+++ lab3_4.py
@@ -1,7 +1,48 @@
-from lab3_1 import newton
+import numpy as np
+import matrix_transformations as mt
+
+def newtons_polynom(x, y, n):
+    # пришлось переписывать полином, чтобы он хранил коэффициенты
+    diff = mt.zeroes(n, n)
+    for i in range(0, n):
+        diff[i][i] = y[i]
+        for j in range(i - 1, -1, -1):
+            diff[j][i] = (diff[j + 1][i] - diff[j][i - 1])
+            diff[j][i] /= (x[i] - x[j])
+    coeff = [diff[0][i] for i in range(n)]
+    return x, coeff, n
 
 
-def derivative(x, y, val, calc_second=True):
+def newtons_derivative(polynom, point, order):
+    # производная от заданного полинома заданной степени
+    x, coeff, n = polynom
+
+    def mul_deriv(operands, cur_order=order):
+        res = 0.0
+        if cur_order != 0:
+            for i in range(n):
+                if operands[i] != 0:
+                    operands[i] = 0
+                    res += mul_deriv(operands, cur_order - 1)
+                    operands[i] = 1
+        else:
+            res = 1.0
+            for i in range(n):
+                if operands[i] != 0:
+                    res *= (point - x[i])
+        return res
+
+    operands = [0.] * n
+    for i in range(order):
+        operands[i] = 1
+    result = 0.0
+    for i in range(order, n):
+        result += coeff[i] * mul_deriv(operands)
+        operands[i] = 1
+
+    return result
+
+def simple_derivative(x, y, val, order):
     n = len(x)
     k = 0
     try:
@@ -13,47 +54,47 @@ def derivative(x, y, val, calc_second=True):
 
     if k + 1 > n + 1:
         k -= 1  # самый последний элемент
-    # апроксимация отрезками прямой
-    first = (y[k + 1] - y[k]) / (x[k + 1] - x[k])
 
-    # апроксимация многочленом второй степени
-    first = first + \
-            ((y[k + 2] - y[k + 1]) / (x[k + 2] - x[k + 1]) - (y[k + 1] - y[k]) / (x[k + 1] - x[k])) * \
-            (2 * val - x[k] - x[k + 1]) / \
-            (x[k + 2] - x[k])
-
-    # апроксимация многочленом Ньютона
-    first_new = newton(val, x, show=False)
-
-    y_diff = [newton(elem, x, show=False) for elem in x]
-
-    if calc_second:
-        _, second_new, _, _ = derivative(x, y_diff, val, calc_second=False)
-        # Для вычисления второй производной, необходимо использовать
-        # интерполяционный многочлен, как минимум второй степени.
-        second = 2 * ((y[k + 2] - y[k + 1]) / (x[k + 2] - x[k + 1]) - (y[k + 1] - y[k]) / (x[k + 1] - x[k])) \
-                 / (x[k + 2] - x[k])
+    if order == 1:
+        # апроксимация отрезками прямой
+        first = (y[k + 1] - y[k]) / (x[k + 1] - x[k])
+        # апроксимация многочленом второй степени
+        first = first + \
+                ((y[k + 2] - y[k + 1]) / (x[k + 2] - x[k + 1]) - (y[k + 1] - y[k]) / (x[k + 1] - x[k])) * \
+                (2 * val - x[k] - x[k + 1]) / \
+                (x[k + 2] - x[k])
+        return first
+    elif order == 2:
+            # Для вычисления второй производной, необходимо использовать
+            # интерполяционный многочлен, как минимум второй степени.
+            second = 2 * ((y[k + 2] - y[k + 1]) / (x[k + 2] - x[k + 1]) - (y[k + 1] - y[k]) / (x[k + 1] - x[k])) \
+                     / (x[k + 2] - x[k])
+            return second
     else:
-        second_new = second = None
-
-    return first, first_new, second, second_new
+        return None
 
 
 def main():
     x = [float(num) for num in input().split(' ')]
     y = [float(num) for num in input().split(' ')]
-    val = float(input())
-    first, first_new, second, second_new = derivative(x, y, val)
-    print("В точке ", val)
-    print("Первая производная многочленом 2 степени", first)
-    print("Первая производная многочленом Ньютона {} степени".format(len(x)), first_new)
-    print("Разница: ", abs(first - first_new))
-    print("Вторая производная многочленом 2 степени", second)
-    print("Вторая производная многочленом Ньютона {} степени".format(len(x)), second_new)
-    print("Разница: ", abs(second - second_new))
-# # TODO: Сделать вычисление производных с помощью многочлена Ньютона
-# Для второй придётся вычислять прозводную от этого многочлена
+    point = float(input())
 
+
+    polynom = newtons_polynom(x,y,len(x))
+
+    # Первая производная
+    print("Первая производная")
+    res = simple_derivative(x, y, point, 1)
+    print("Приближение по 2 точкам",res)
+    res = newtons_derivative(polynom, point, 1 )
+    print("Приближение м-м Ньютона", res)
+    print()
+    print("Вторая производная")
+    polynom = newtons_polynom(x,y,len(x))
+    res = newtons_derivative(polynom, point, 2)
+    print("Приближение по 2 точкам",res)
+    res = simple_derivative(x, y, point, 2)
+    print("Приближение м-м Ньютона", res)
 
 if __name__ == '__main__':
     main()
